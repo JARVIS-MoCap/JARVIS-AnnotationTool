75262e686d04ece4a0199cd2630c2f70272cf4fc
mocapsetup <Thueser@dpz.eu>
2020-12-11 18:26:30 +0100

    added macOS support

--- main.cpp
+++ main.cpp
@@ -14,11 +14,28 @@
 #include <QSettings>
 #include <QImage>
 
+#ifdef __APPLE__
+#include "CoreFoundation/CoreFoundation.h"
+#endif
 
 Q_DECLARE_METATYPE(QList<int>)
 
 int main(int argc, char **argv)
 {
+	// This makes relative paths work in C++ in Xcode by changing directory to the Resources folder inside the .app bundle
+	#ifdef __APPLE__
+			CFBundleRef mainBundle = CFBundleGetMainBundle();
+			CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+			char path[PATH_MAX];
+			if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
+			{
+					// error!
+			}
+			CFRelease(resourcesURL);
+
+			chdir(path);
+			std::cout << "Current Path: " << path << std::endl;
+	#endif
 	QCoreApplication::setOrganizationName("Test");
 	QCoreApplication::setOrganizationDomain("test");
 	QCoreApplication::setApplicationName("Annotation Tool");
@@ -29,7 +46,7 @@ int main(int argc, char **argv)
 	QApplication app (argc, argv);
 	app.setStyle(new DarkStyle);
 
-	QStringList themeSearchPaths = {"/home/trackingsetup/Documents/AnnotationToolbox/IconThemes"};
+	QStringList themeSearchPaths = {"/home/trackingsetup/Documents/AnnotationToolbox/IconThemes", "IconThemes"};
 	QIcon::setThemeSearchPaths(themeSearchPaths);
 	QIcon::setThemeName("DarkIconTheme");
 	MainWindow m_window;
