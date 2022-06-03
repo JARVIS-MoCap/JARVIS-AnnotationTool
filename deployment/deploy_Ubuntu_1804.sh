9656cbae10b3387dff5069f3dc4a41c5cd4f3d4d
timohueser <timo.hueser@gmail>
2022-06-03 11:44:56 +0200

    minor fix deploy script 1804

--- deployment/deploy_Ubuntu_1804.sh
+++ deployment/deploy_Ubuntu_1804.sh
@@ -4,5 +4,5 @@ cmake -DUBUNTU_VERSION=1804 ..
 make -j12
 make install
 cd ../deployment
-dpkg-deb --build --root-owner-group deb_packages/JARVIS-AnnotationTool_0.3-1_amd64_1804
-mv deb_packages/JARVIS-AnnotationTool_0.3-1_amd64_1804.deb .
+dpkg-deb --build --root-owner-group deb_packages/JARVIS-AnnotationTool_1.1-1_amd64_1804
+mv deb_packages/JARVIS-AnnotationTool_1.1-1_amd64_1804.deb .
