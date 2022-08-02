6bc4f448e972d2b933649b07704b3e1b596309b5
timo.hueser <timo.hueser@gmail.com>
2022-08-02 21:45:28 +0200

    Setup Scripts work on windows and linux

--- setup.sh
+++ setup.sh
@@ -15,33 +15,33 @@ cd libs/OpenCV
 mkdir build
 cd build
 
-# if [ "${machine}" = "Linux" ];
-# then
-#   cmake -DOPENCV_ENABLE_ALLOCATOR_STATS=OFF -DCMAKE_BUILD_TYPE=RELEASE \
-#         -DBUILD_TIFF=OFF -DWITH_TIFF=OFF -DBUILD_JPEG=OFF -DBUILD_ZLIB=OFF \
-#         -DBUILD_WEBP=OFF -DBUILD_PNG=OFF -DWITH_OPENEXR=OFF -DWITH_OPENJPEG=OFF \
-#         -DWITH_JASPER=OFF -DWITH_PROTOBUF=OFF -DWITH_QUIRC=OFF -DWITH_1394=OFF \
-#         -DWITH_V4L=OFF  -DWITH_GSTREAMER=ON -DWITH_FFMPEG=ON -DWITH_GTK=OFF \
-#         -DBUILD_SHARED_LIBS=OFF \
-#         -DBUILD_LIST="core,calib3d,imgproc,videoio,aruco, gapi" \
-#         -DCMAKE_INSTALL_PREFIX=../opencv_static \
-#         -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
-#   cmake --build . -parallel
-#   cmake --install .
-# fi
+if [ "${machine}" = "Linux" ];
+then
+  cmake -DOPENCV_ENABLE_ALLOCATOR_STATS=OFF -DCMAKE_BUILD_TYPE=RELEASE \
+        -DBUILD_TIFF=OFF -DWITH_TIFF=OFF -DBUILD_JPEG=OFF -DBUILD_ZLIB=OFF \
+        -DBUILD_WEBP=OFF -DBUILD_PNG=OFF -DWITH_OPENEXR=OFF -DWITH_OPENJPEG=OFF \
+        -DWITH_JASPER=OFF -DWITH_PROTOBUF=OFF -DWITH_QUIRC=OFF -DWITH_1394=OFF \
+        -DWITH_V4L=OFF  -DWITH_GSTREAMER=ON -DWITH_FFMPEG=ON -DWITH_GTK=OFF \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_LIST="core,calib3d,imgproc,videoio,aruco, gapi" \
+        -DCMAKE_INSTALL_PREFIX=../opencv_install \
+        -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../opencv
+  cmake --build . --parallel 8
+  cmake --install .
+fi
 
 cd ../../
 
 cd Qt5/qt5
 pwd
-./init-repository
+./init-repository -f --module-subset=qtbase,qtsvg,qtmultimedia,qttools,qttranslations,qtdoc,qtrepotools,qtqa,qtwayland,qt3d,qtimageformats,qtserialbus,qtserialport,qtcharts,qtdatavis3d,qtshadertools
 cd ..
 mkdir build
 cd build
 
 if [ "${machine}" = "Linux" ];
 then
-  ../qt5/configure -static -release -no-pch -prefix ../qt_static -opensource \
+  ../qt5/configure -static -release -no-pch -prefix ../qt_install -opensource \
         -confirm-license -nomake tools -nomake tests -nomake examples \
         -skip webengine -skip qtandroidextras -skip qtconnectivity  \
         -skip qtdeclarative -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects \
@@ -52,10 +52,10 @@ then
         -skip qtwebchannel -skip qtwebengine -skip qtwebsockets \
         -skip qtwebview -skip qtwinextras -skip qtxmlpatterns -skip qtwebglplugin \
         -no-openssl -skip qtlottie -skip qtmqtt -skip qtopcua -skip qtquicktimeline \
-        -skip qtquick3d 
+        -skip qtquick3d
 
-  # cmake --build . --parallel
-  # cmake --install .
+  cmake --build . --parallel 8
+  cmake --install .
 fi
 
 cd ../../../
