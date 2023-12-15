4dfe90e09b3e9dc83fe55492e6c754ea3d5672eb
jarvis <jarvismovap@gmail.com>
2023-12-15 12:18:54 +0100

    buped release to 1.3

--- deployment/deploy_Ubuntu_2204.sh
+++ deployment/deploy_Ubuntu_2204.sh
@@ -4,5 +4,5 @@ cmake -DUBUNTU_VERSION=2204 -DCMAKE_BUILD_TYPE=RELEASE ..
 cmake --build . --parallel 8
 cmake --install .
 cd ../deployment
-dpkg-deb --build --root-owner-group deb_packages/JARVIS-AnnotationTool_1.2-1_amd64_2204
-mv deb_packages/JARVIS-AnnotationTool_1.2-1_amd64_2204.deb .
+dpkg-deb --build --root-owner-group deb_packages/JARVIS-AnnotationTool_1.3-1_amd64_2204
+mv deb_packages/JARVIS-AnnotationTool_1.3-1_amd64_2204.deb .
